55c5439c1ebb8447981a50c617196e02f2ebba0e
mattflax <matt@flax.co.uk>
2015-03-31 09:30:30 +0100

    Modifications for Solr 5.0 on Linux.

--- spot/spot-ontology/ontology_solr.sh
+++ spot/spot-ontology/ontology_solr.sh
@@ -1,6 +1,5 @@
-SOLR_DIR=/home/mlp/apps/solr-4.10.1/example
+SOLR_DIR=/home/mlp/apps/solr-5.0.0
 SOLR_HOME=/home/mlp/flax/BioSolr/spot/spot-ontology/solr-conf
 
-cd $SOLR_DIR
-java -Dsolr.solr.home=$SOLR_HOME -jar start.jar
-
+cd $SOLR_DIR/bin
+. solr start -s $SOLR_HOME
