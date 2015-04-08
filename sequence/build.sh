178f6edfac92eca3b4f67c192d0c961dbc9211fa
Tom Winch <tomw@flax.co.uk>
2015-04-08 09:54:16 +0100

    Ensure SimpleXJoinResultsFactory makes it into the fasta.jar

--- sequence/build.sh
+++ sequence/build.sh
@@ -1,5 +1,5 @@
 cd bin
-jar cvfe fasta.jar uk.co.flax.biosolr.pdbe.Main uk
+jar cvfe fasta.jar uk.co.flax.biosolr.pdbe.Main uk org/apache/solr/search/xjoin/SimpleXJoinResultsFactory.class
 jar cvf xjoin.jar org/apache/solr/search/xjoin/FieldAppender.class org/apache/solr/search/xjoin/NameConverter.class org/apache/solr/search/xjoin/XJoin*.class 
 cd ..
 mv bin/*.jar .
