0eedac2f9335b5c18e00897803d8deafe87c939c
Matt Pearce <matt@flax.co.uk>
2015-05-12 15:33:54 +0100

    Copyright notices.

--- spot/solr-hierarchical-facet/src/main/java/uk/co/flax/biosolr/FacetTreeGenerator.java
+++ spot/solr-hierarchical-facet/src/main/java/uk/co/flax/biosolr/FacetTreeGenerator.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2015 Lemur Consulting Ltd.
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.co.flax.biosolr;
 
 import java.io.IOException;
@@ -38,6 +53,9 @@ import org.apache.solr.util.RefCounted;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Class to generate a facet tree.
+ */
 public class FacetTreeGenerator {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(FacetTreeGenerator.class);
