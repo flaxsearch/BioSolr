e29e567863aba942d48b7f3a5ab42285cd24674d
Tony Burdett <tburdett@ebi.ac.uk>
2015-07-01 13:22:07 +0100

    adding demo web application

---
+++ demo/biosolr-demo/src/main/java/uk/ac/ebi/spot/biosolr/BioSolrDemoApplication.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.spot.biosolr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioSolrDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BioSolrDemoApplication.class, args);
+    }
+}
---
+++ demo/biosolr-demo/src/main/java/uk/ac/ebi/spot/biosolr/WebConfiguration.java
@@ -0,0 +1,10 @@
+package uk.ac.ebi.spot.biosolr;
+
+/**
+ * Javadocs go here!
+ *
+ * @author Tony Burdett
+ * @date 26/06/15
+ */
+public class WebConfiguration {
+}
