b8c79ece1583d7f4e9dd0016b1248b146c725341
Matt Pearce <matt@flax.co.uk>
2015-08-20 11:28:10 +0100

    Change label lookups to allow multiple label URIs.

--- spot/solr-ontology-updateprocessor/src/main/java/uk/co/flax/biosolr/OntologyHelper.java
+++ spot/solr-ontology-updateprocessor/src/main/java/uk/co/flax/biosolr/OntologyHelper.java
@@ -25,6 +25,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
@@ -35,6 +36,7 @@ import org.semanticweb.owlapi.model.OWLAnnotationProperty;
 import org.semanticweb.owlapi.model.OWLAnnotationValue;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLLiteral;
 import org.semanticweb.owlapi.model.OWLObjectProperty;
 import org.semanticweb.owlapi.model.OWLObjectSomeValuesFrom;
@@ -46,7 +48,6 @@ import org.semanticweb.owlapi.reasoner.Node;
 import org.semanticweb.owlapi.reasoner.NodeSet;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.structural.StructuralReasonerFactory;
-import org.semanticweb.owlapi.vocab.OWLRDFVocabulary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -59,9 +60,9 @@ import org.slf4j.LoggerFactory;
 public class OntologyHelper {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(OntologyHelper.class);
-	
+
 	private final OntologyConfiguration config;
-	
+
 	private final OWLOntology ontology;
 	private final OWLReasoner reasoner;
 //	private final ShortFormProvider shortFormProvider;
@@ -70,40 +71,54 @@ public class OntologyHelper {
 	private final Map<IRI, OWLClass> owlClassMap = new HashMap<>();
 
 	private Map<IRI, Collection<String>> labels = new HashMap<>();
-	
+	private Map<IRI, Collection<String>> synonyms = new HashMap<>();
+	private Map<IRI, Collection<String>> definitions = new HashMap<>();
+
 	/**
-	 * Construct a new ontology helper instance with a string representing
-	 * the ontology URI.
-	 * @param ontologyUriString the URI.
-	 * @param config the ontology configuration, containing the property URIs
-	 * for labels, synonyms, etc.
-	 * @throws OWLOntologyCreationException if the ontology cannot be read
-	 * for some reason - internal inconsistencies, etc.
-	 * @throws URISyntaxException if the URI cannot be parsed.
+	 * Construct a new ontology helper instance with a string representing the
+	 * ontology URI.
+	 * 
+	 * @param ontologyUriString
+	 *            the URI.
+	 * @param config
+	 *            the ontology configuration, containing the property URIs for
+	 *            labels, synonyms, etc.
+	 * @throws OWLOntologyCreationException
+	 *             if the ontology cannot be read for some reason - internal
+	 *             inconsistencies, etc.
+	 * @throws URISyntaxException
+	 *             if the URI cannot be parsed.
 	 */
-	public OntologyHelper(String ontologyUriString, OntologyConfiguration config) throws OWLOntologyCreationException, URISyntaxException {
+	public OntologyHelper(String ontologyUriString, OntologyConfiguration config) throws OWLOntologyCreationException,
+			URISyntaxException {
 		this(new URI(ontologyUriString), config);
 	}
 
 	/**
 	 * Construct a new ontology helper instance.
-	 * @param ontologyUri the URI giving the location of the ontology.
-	 * @param config the ontology configuration, containing the property URIs
-	 * for labels, synonyms, etc.
-	 * @throws OWLOntologyCreationException if the ontology cannot be read
-	 * for some reason - internal inconsistencies, etc.
-	 * @throws URISyntaxException if the URI cannot be parsed.
+	 * 
+	 * @param ontologyUri
+	 *            the URI giving the location of the ontology.
+	 * @param config
+	 *            the ontology configuration, containing the property URIs for
+	 *            labels, synonyms, etc.
+	 * @throws OWLOntologyCreationException
+	 *             if the ontology cannot be read for some reason - internal
+	 *             inconsistencies, etc.
+	 * @throws URISyntaxException
+	 *             if the URI cannot be parsed.
 	 */
-	public OntologyHelper(URI ontologyUri, OntologyConfiguration config) throws OWLOntologyCreationException, URISyntaxException {
+	public OntologyHelper(URI ontologyUri, OntologyConfiguration config) throws OWLOntologyCreationException,
+			URISyntaxException {
 		this.config = config;
-		
+
 		if (!ontologyUri.isAbsolute()) {
 			// Try to read as a file from the resource path
 			LOGGER.debug("Ontology URI {} is not absolute - loading from classpath", ontologyUri);
 			ontologyUri = this.getClass().getClassLoader().getResource(ontologyUri.toString()).toURI();
 		}
 		LOGGER.info("Loading ontology from " + ontologyUri + "...");
-		
+
 		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
 		IRI iri = IRI.create(ontologyUri);
 		this.ontology = manager.loadOntologyFromOntologyDocument(iri);
@@ -123,7 +138,7 @@ public class OntologyHelper {
 	}
 
 	/**
-	 * Explicitly dispose of the helper class, closing down any resources in 
+	 * Explicitly dispose of the helper class, closing down any resources in
 	 * use.
 	 */
 	public void dispose() {
@@ -132,32 +147,38 @@ public class OntologyHelper {
 
 	/**
 	 * Get the OWL class for an IRI.
-	 * @param iri the IRI of the required class.
+	 * 
+	 * @param iri
+	 *            the IRI of the required class.
 	 * @return the class from the ontology, or <code>null</code> if no such
-	 * class can be found, or the IRI string is null.
+	 *         class can be found, or the IRI string is null.
 	 */
 	public OWLClass getOwlClass(String iri) {
 		OWLClass ret = null;
-		
+
 		if (StringUtils.isNotBlank(iri)) {
 			ret = owlClassMap.get(IRI.create(iri));
 		}
-		
+
 		return ret;
 	}
 
 	/**
 	 * Find the labels for a single OWL class.
-	 * @param owlClass the class whose labels are required.
+	 * 
+	 * @param owlClass
+	 *            the class whose labels are required.
 	 * @return a collection of labels for the class.
 	 */
 	public Collection<String> findLabels(OWLClass owlClass) {
 		return findLabels(owlClass.getIRI());
 	}
-	
+
 	/**
 	 * Find all of the labels for a collection of OWL class IRIs.
-	 * @param iris the IRIs whose labels should be looked up.
+	 * 
+	 * @param iris
+	 *            the IRIs whose labels should be looked up.
 	 * @return a collection of labels. Never <code>null</code>.
 	 */
 	public Collection<String> findLabelsForIRIs(Collection<String> iris) {
@@ -170,17 +191,18 @@ public class OntologyHelper {
 		Set<String> classNames = new HashSet<>();
 
 		if (!labels.containsKey(iri)) {
-			// get label annotation property
-			OWLAnnotationProperty labelAnnotationProperty = ontology.getOWLOntologyManager().getOWLDataFactory()
-					.getOWLAnnotationProperty(OWLRDFVocabulary.RDFS_LABEL.getIRI());
+			OWLDataFactory odf = ontology.getOWLOntologyManager().getOWLDataFactory();
 
-			classNames = new HashSet<>(findAnnotationNames(iri, labelAnnotationProperty));
+			// For every label property URI, find the annotations for this entry
+			config.getLabelPropertyUris().parallelStream().map(uri -> odf.getOWLAnnotationProperty(IRI.create(uri)))
+					.map(prop -> findAnnotationNames(iri, prop)).forEach(classNames::addAll);
+
 			labels.put(iri, classNames);
 		}
 
 		return labels.get(iri);
 	}
-
+	
 	private Collection<String> findAnnotationNames(IRI iri, OWLAnnotationProperty annotationType) {
 		Collection<String> classNames = new HashSet<String>();
 
@@ -188,8 +210,9 @@ public class OntologyHelper {
 		for (OWLAnnotationAssertionAxiom axiom : ontology.getAnnotationAssertionAxioms(iri)) {
 			if (axiom.getAnnotation().getProperty().equals(annotationType)) {
 				OWLAnnotationValue value = axiom.getAnnotation().getValue();
-				if (value instanceof OWLLiteral) {
-					classNames.add(((OWLLiteral) value).getLiteral());
+				Optional<String> name = getOWLAnnotationValueAsString(value);
+				if (name.isPresent()) {
+					classNames.add(name.get());
 				}
 			}
 		}
@@ -199,85 +222,92 @@ public class OntologyHelper {
 
 	/**
 	 * Get the direct child URIs for an OWL class.
+	 * 
 	 * @param owlClass
 	 * @return the child URIs, as strings. Never <code>null</code>.
 	 */
 	public Collection<String> getChildUris(OWLClass owlClass) {
-    	return getSubclassUris(owlClass, true);
-    }
+		return getSubclassUris(owlClass, true);
+	}
 
 	/**
 	 * Get all descendent URIs for an OWL class, including direct children.
+	 * 
 	 * @param owlClass
 	 * @return the descendent URIs, as strings. Never <code>null</code>.
 	 */
 	public Collection<String> getDescendentUris(OWLClass owlClass) {
-    	return getSubclassUris(owlClass, false);
-    }
-	
+		return getSubclassUris(owlClass, false);
+	}
+
 	/**
 	 * Get direct parent URIs for an OWL class.
+	 * 
 	 * @param owlClass
 	 * @return the parent URIs, as strings. Never <code>null</code>.
 	 */
-    public Collection<String> getParentUris(OWLClass owlClass) {
-    	return getSuperclassUris(owlClass, true);
-    }
-    
-    /**
-     * Get all ancestor URIs for an OWL class, including direct parents.
-     * @param owlClass
-     * @return the ancestor URIs, as strings. Never <code>null</code>.
-     */
-    public Collection<String> getAncestorUris(OWLClass owlClass) {
-    	return getSuperclassUris(owlClass, false);
-    }
-    
+	public Collection<String> getParentUris(OWLClass owlClass) {
+		return getSuperclassUris(owlClass, true);
+	}
+
+	/**
+	 * Get all ancestor URIs for an OWL class, including direct parents.
+	 * 
+	 * @param owlClass
+	 * @return the ancestor URIs, as strings. Never <code>null</code>.
+	 */
+	public Collection<String> getAncestorUris(OWLClass owlClass) {
+		return getSuperclassUris(owlClass, false);
+	}
+
 	private Collection<String> getSubclassUris(OWLClass owlClass, boolean direct) {
-    	return getUrisFromNodeSet(reasoner.getSubClasses(owlClass, direct));
+		return getUrisFromNodeSet(reasoner.getSubClasses(owlClass, direct));
 	}
 
 	private Collection<String> getSuperclassUris(OWLClass owlClass, boolean direct) {
-    	return getUrisFromNodeSet(reasoner.getSuperClasses(owlClass, direct));
+		return getUrisFromNodeSet(reasoner.getSuperClasses(owlClass, direct));
 	}
 
-    private Collection<String> getUrisFromNodeSet(NodeSet<OWLClass> nodeSet) {
-    	Set<String> uris = new HashSet<>();
-    	
-    	for (Node<OWLClass> node : nodeSet) {
-    		for (OWLClass expr : node.getEntities()) {
-    			if (isClassSatisfiable(expr)) {
-    				uris.add(expr.getIRI().toURI().toString());
-    			}
-    		}
-    	}
-    	
-    	return uris;
-    }
-    
-    private boolean isClassSatisfiable(OWLClass owlClass) {
-    	return !owlClass.isAnonymous() && !owlClass.getIRI().equals(owlNothingIRI);
-    }
-    
-    /**
-     * Retrieve a map of related classes for a particular class.
-     * @param owlClass
-     * @return a map of relation type to a list of IRIs for nodes with that relationship.
-     */
-    public Map<String, List<String>> getRestrictions(OWLClass owlClass) {
-    	RestrictionVisitor visitor = new RestrictionVisitor(Collections.singleton(ontology));
+	private Collection<String> getUrisFromNodeSet(NodeSet<OWLClass> nodeSet) {
+		Set<String> uris = new HashSet<>();
+
+		for (Node<OWLClass> node : nodeSet) {
+			for (OWLClass expr : node.getEntities()) {
+				if (isClassSatisfiable(expr)) {
+					uris.add(expr.getIRI().toURI().toString());
+				}
+			}
+		}
+
+		return uris;
+	}
+
+	private boolean isClassSatisfiable(OWLClass owlClass) {
+		return !owlClass.isAnonymous() && !owlClass.getIRI().equals(owlNothingIRI);
+	}
+
+	/**
+	 * Retrieve a map of related classes for a particular class.
+	 * 
+	 * @param owlClass
+	 * @return a map of relation type to a list of IRIs for nodes with that
+	 *         relationship.
+	 */
+	public Map<String, List<String>> getRestrictions(OWLClass owlClass) {
+		RestrictionVisitor visitor = new RestrictionVisitor(Collections.singleton(ontology));
 		for (OWLSubClassOfAxiom ax : ontology.getSubClassAxiomsForSubClass(owlClass)) {
 			OWLClassExpression superCls = ax.getSuperClass();
 			// Ask our superclass to accept a visit from the RestrictionVisitor
-			// - if it is an existential restriction then our restriction visitor
+			// - if it is an existential restriction then our restriction
+			// visitor
 			// will answer it - if not our visitor will ignore it
 			superCls.accept(visitor);
 		}
-		
+
 		Map<String, List<String>> restrictions = new HashMap<>();
 		for (OWLObjectSomeValuesFrom val : visitor.getSomeValues()) {
 			OWLClassExpression exp = val.getFiller();
-			
+
 			// Get the shortname of the property expression
 			String shortForm = null;
 			Set<OWLObjectProperty> signatureProps = val.getProperty().getObjectPropertiesInSignature();
@@ -290,15 +320,57 @@ public class OntologyHelper {
 
 			if (shortForm != null && !exp.isAnonymous()) {
 				IRI iri = exp.asOWLClass().getIRI();
-				
+
 				if (!restrictions.containsKey(shortForm)) {
 					restrictions.put(shortForm, new ArrayList<String>());
 				}
 				restrictions.get(shortForm).add(iri.toString());
 			}
 		}
-		
+
 		return restrictions;
-    }
-    
+	}
+
+	private Optional<String> getOWLAnnotationValueAsString(OWLAnnotationValue value) {
+		if (value instanceof IRI) {
+			Optional<String> shortForm = getShortForm((IRI) value);
+			if (shortForm.isPresent()) {
+				return Optional.of(shortForm.get());
+			}
+		} else if (value instanceof OWLLiteral) {
+			return Optional.of(((OWLLiteral) value).getLiteral());
+		}
+		return Optional.empty();
+	}
+
+	private Optional<String> getShortForm(IRI entityIRI) {
+		LOGGER.trace("Attempting to extract fragment name of URI '" + entityIRI + "'");
+		String termURI = entityIRI.toString();
+		URI entUri = entityIRI.toURI();
+
+		// we want the "final part" of the URI...
+		if (!StringUtils.isEmpty(entUri.getFragment())) {
+			// a uri with a non-null fragment, so use this...
+			LOGGER.trace("Extracting fragment name using URI fragment (" + entUri.getFragment() + ")");
+			return Optional.of(entUri.getFragment());
+		} else if (entityIRI.toURI().getPath() != null) {
+			// no fragment, but there is a path so try and extract the final
+			// part...
+			if (entityIRI.toURI().getPath().contains("/")) {
+				LOGGER.trace("Extracting fragment name using final part of the path of the URI");
+				return Optional.of(entityIRI.toURI().getPath()
+						.substring(entityIRI.toURI().getPath().lastIndexOf('/') + 1));
+			} else {
+				// no final path part, so just return whole path
+				LOGGER.trace("Extracting fragment name using the path of the URI");
+				return Optional.of(entityIRI.toURI().getPath());
+			}
+		} else {
+			// no fragment, path is null, we've run out of rules so don't
+			// shorten
+			LOGGER.trace("No rules to shorten this URI could be found (" + termURI + ")");
+			return Optional.empty();
+		}
+	}
+
 }
