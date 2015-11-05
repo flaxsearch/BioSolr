8204e9d14fa5aa40695f6f1dde9f2976c2e3097d
Tom Winch <tomw@flax.co.uk>
2015-11-05 09:20:20 +0000

    Fixes for patch precommit

--- federated/src/java/org/apache/solr/search/djoin/FilterQParserSearchComponent.java
+++ federated/src/java/org/apache/solr/search/djoin/FilterQParserSearchComponent.java
@@ -1,5 +1,22 @@
 package org.apache.solr.search.djoin;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import java.io.IOException;
 import java.util.Set;
 
@@ -13,19 +30,19 @@ import org.apache.solr.search.SyntaxError;
 
 public class FilterQParserSearchComponent extends SearchComponent {
 
-	public static final String COMPONENT_NAME = "filter";
+  public static final String COMPONENT_NAME = "filter";
 
-	// initialisation parameters
-	public static final String INIT_QPARSER = "qParser";
-	
-	private String qParser;
+  // initialisation parameters
+  public static final String INIT_QPARSER = "qParser";
+  
+  private String qParser;
 
   @Override
   @SuppressWarnings("rawtypes")
   public void init(NamedList args) {
-  	super.init(args);
+    super.init(args);
   
-  	qParser = (String)args.get(INIT_QPARSER);
+    qParser = (String)args.get(INIT_QPARSER);
   }
 
   @Override
@@ -37,32 +54,32 @@ public class FilterQParserSearchComponent extends SearchComponent {
   public void process(ResponseBuilder rb) throws IOException {
     // do nothing (not called in aggregator)
   }
-	
-	@Override
-	public void modifyRequest(ResponseBuilder rb, SearchComponent who, ShardRequest sreq) {
-	  Set<String> names = sreq.params.getParameterNames();
-	  for (String name : names.toArray(new String[names.size()])) {
-	    for (String value : sreq.params.getParams(name)) {
-	      try {
-  	      SolrParams params = QueryParsing.getLocalParams(value, sreq.params);
-  	      if (params != null && params.get("type").equals(qParser)) {
-  	        sreq.params.remove(name, value);
-  	      }
-	      } catch (SyntaxError e) {
-	        // ignore
-	      }
-	    }
-	  }
-	}
-	
-	@Override
-	public String getDescription() {
-		return "$description";
-	}
+  
+  @Override
+  public void modifyRequest(ResponseBuilder rb, SearchComponent who, ShardRequest sreq) {
+    Set<String> names = sreq.params.getParameterNames();
+    for (String name : names.toArray(new String[names.size()])) {
+      for (String value : sreq.params.getParams(name)) {
+        try {
+          SolrParams params = QueryParsing.getLocalParams(value, sreq.params);
+          if (params != null && params.get("type").equals(qParser)) {
+            sreq.params.remove(name, value);
+          }
+        } catch (SyntaxError e) {
+          // ignore
+        }
+      }
+    }
+  }
+  
+  @Override
+  public String getDescription() {
+    return "$description";
+  }
 
-	@Override
-	public String getSource() {
-		return "$source";
-	}
+  @Override
+  public String getSource() {
+    return "$source";
+  }
 
 }
