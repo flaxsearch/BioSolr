78e98fa34625cae52a553bc3a9ba774b01dc8e5e
Tony Burdett <tburdett@ebi.ac.uk>
2015-12-03 09:34:17 +0000

    removing local path from script, updating to use user set variable

--- swat4ls_demo/solr_start.sh
+++ swat4ls_demo/solr_start.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-SOLR_DIR=/home/mlp/apps/solr
 SOLR_HOME=./solr-conf
 
 DEBUG_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044"
 
-# . solr start -s $SOLR_HOME
-$SOLR_DIR/bin/solr start -s $SOLR_HOME -a "${DEBUG_OPTS}"
+# check SOLR_DIR has been set
+if [ -z $SOLR_DIR ]
+  then
+    echo '$SOLR_DIR not set - please set this to the location of your Solr installation' >&2
+  else
+    echo 'Starting Solr using SOLR_HOME = $SOLR_HOME'
+    $SOLR_DIR/bin/solr start -s $SOLR_HOME -a "${DEBUG_OPTS}"
+    exit 1
+fi
