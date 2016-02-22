7aa65027061f1371cda4c491d1dd8c25ab6afd11
Matt Pearce <matt@flax.co.uk>
2016-02-22 16:32:08 +0000

    Build grouped document search for ES.

--- swat4ls_demo/elasticsearch/README.md
+++ swat4ls_demo/elasticsearch/README.md
@@ -86,7 +86,45 @@ We can also inspect some of the data by running a query [http://localhost:9200/b
 correspond to rows in our spreadsheet.
 
 
-## Part Three - Install BioSolr plugin
+## Part Three - Run the sample web application
+
+Next, we've supplied you with a simple web application to search our new Solr index.  Let's try running this.
+
+```
+>: cd ~/Projects/BioSolr/swat4ls_demo/tools
+>: java -jar swat4ls-webapp-1.0-SNAPSHOT.jar server webapp_es.yml
+```
+
+You should see a bunch of logging information as the web application starts up. If all goes well, you'll eventually see a message like this:
+```
+INFO  [2015-12-06 18:50:49,002] org.eclipse.jetty.server.Server: Started @1637ms
+```
+
+Now we can open up our example application by browsing to [http://localhost:8080/](http://localhost:8080/).
+
+Let's try some example searches.  Most of the GWAS data is concerned with the links between SNPs and diseases - so let's try a search for `Lung cancer`.
+Looks like we get 31 results, all containing lung cancer in the title or the association line, so for example:
+
+> 1.    **Deciphering the impact of common genetic variation on lung cancer risk: a genome-wide association study.**  
+>       Broderick P - Cancer Res.  
+>       *rs4254535* is associated with *Lung cancer*  
+
+But what if we want all lung diseases? We could try searching for `lung disease` - but this only gives us 3 results, probably not what we want.  We can try just searching for `lung`, which looks a little better - 49 results this time, some of them are lung cancer but there's also stuff about lung function, so this isn't ideal either.
+
+Let's try another example.  We could search for `schizophrenia` - this gives us nice results, we get 51 documents that seems to be annotated to schizophrenia or treatment responses in schizophrenia.  But what if we want other mental disorders?  If we search for `mental disorder` we get no results.
+
+BioSolr's ontology expansion plugin will help us improve these search results a lot.  So let's kill our search web application and move on to installing BioSolr.
+
+#### Additional Tasks
+
+- [x] Try searching for other diseases that you might expect to find results for
+- [x] What happens if you search by other anatomical features? Try `heart` or `liver`
+- [x] Does checking the boxes to include parent or child labels help?
+- [x] Can you find measurements?  For example, you could try searching for `blood markers`
+- [x] You can explore the source code for this webapp by looking in the `webapp/` directory of this Github repository.
+ 
+
+## Part Four - Install BioSolr plugin
 
 Now we're going to try and improve our search results using the structure of the ontology.  To do this, we need to add the BioSolr ontology expansion plugin.
 
@@ -103,7 +141,7 @@ Now install the BioSolr plugin from the `plugins` directory.
 We've installed our plugin, now we need to tell ElasticSearch when to use it.
 
 
-## Part Four - Configure BioSolr
+## Part Five - Configure BioSolr
 
 Start up ElasticSearch again.
 
@@ -127,7 +165,7 @@ you will see that efo_uri has a type "ontology", and a number of
 sub-properties defined, including label, synonyms, and so on.
 
 
-## Part Five - Reindex our data to take advantage of ontology enrichment
+## Part Six - Reindex our data to take advantage of ontology enrichment
 
 This bit is simple - we can just rerun our indexing process from earlier...
 
@@ -145,6 +183,63 @@ we should still have 26,385 documents indexed. If we do a query, though -
 - you should see a lot more information than we had earlier, 
 including some new fields that weren't in our spreadsheet.
 
+
+## Part Seven - Ontology-powered search!
+
+Now let's go back to our web application and see if we can take advantage of all this extra information.
+
+Restart the application again:
+```
+>: cd ~/Projects/BioSolr/swat4ls_demo/tools
+>: java -jar swat4ls-webapp-1.0-SNAPSHOT.jar server webapp_es.yml
+```
+You'll straight away notice something new - lots of additional checkboxes (you might need to reload your page).  These are present because our webapp has noticed that we have additional ontology fields in our data.
+
+Now let's go back to our earlier searches.  If you remember, we tried looking for `lung cancer` and we got 31 results.  We should be able to do the same search again, and get the same results.
+
+Then, we tried `lung disease` and only got 3 results.  Again, we should be able to verify this. But now let's check the box to use ontology expansion:
+- [x] Include parent labels
+Now if we rerun the search, we should see 53 results, across a whole variety of lung disease.  Our top hit, for example, should look like this:
+
+> 1.    **Variants in FAM13A are associated with chronic obstructive pulmonary disease.**  
+>       Cho MH - Nat Genet.  
+>       *rs7671167* is associated with *Chronic obstructive pulmonary disease.*   
+>       **Annotation** chronic obstructive pulmonary disease [http://www.ebi.ac.uk/efo/EFO_0000341].  
+>       **Children** chronic bronchitis.  
+>       **Parent(s)** lung disease.  
+>       **Has disease location** trachea lung.  
+
+This looks much more like it! But we can even go one better than this - let's try searching for `lung` again. Uncheck all the boxes so we get 49 results.
+This time, though, let's also include diseases which are located in the lung...
+- [x] "Has disease location"
+Now you should see that we have 75 results; we're using relationships in the ontology to improve our results.  For example, one of our results looks like this:
+
+> 10.   **Genome-wide association study identifies BICD1 as a susceptibility gene for emphysema.**  
+>       Kong X - Am J Respir Crit Care Med.  
+>       *rs641525* is associated with *Emphysema-related traits.*  
+>       **Annotation** emphysema [http://www.ebi.ac.uk/efo/EFO_0000464].  
+>       **Parent(s)** lung disease.  
+>       **Has disease location** lung.  
+
+If you look closely, you'll see that "lung" is not mentioned anywhere in our data, only the extra fields that have come from the ontology.  We'd actually have picked this result up by including parents (`lung disease`), but this is only because EFO nicely defines hierarchy.  If we were using a different ontology with a different hierarchy (maybe one which doesn't use hierarchy in the ways we'd like), we can use a relationship other than `is a` to find this result.
+
+Next, we tried searching for `schizophrenia`.  Let's try this again - yep, still 51 results.  You'll notice if we include parent terms, we still get 51 results - our order might shuffle around a bit though. 
+This isn't unexpected - most of our data about schizophrenia should be nicely mapped to a specific term and would include the text "schizophrenia" in the title or the annotation line.
+But last time we tried to search for other `mental disorders` and found no results at all.  Now, if we search including child labels, we got 150 results! This covers a wide range of disorders, like "schizophrenia", "bipolar disorder" and many more.  For example:
+
+> 1.    **Cross-disorder genomewide analysis of schizophrenia, bipolar disorder, and depression.**  
+>       Huang J - Am J Psychiatry  
+>       *rs1001021* is associated with *Schizophrenia, bipolar disorder and depression (combined)*  
+>       **Annotation** bipolar disorder [http://www.ebi.ac.uk/efo/EFO_0000289]  
+>       **Parent(s)** mental or behavioural disorder  
+
+This shows the power of including additional information from the ontology in your Solr index. You'll also see the search is just as fast as it was previously: by including extra data when we built our index, we have almost no penalty in search time - which is usually the best option for users.
+
+#### Additional Tasks
+
+- [x] Play around with the index some more
+- [x] Can you redo the searches for anatomical features from earlier?  What happens if you search by `heart` or `liver` and include child terms?
+- [x] Try checking extra boxes to include additional relations.  Can you find more ways to search the data?
 
 
 ## Conclusions
--- swat4ls_demo/webapp/src/main/java/uk/co/flax/biosolr/ontology/search/elasticsearch/ElasticDocumentSearch.java
+++ swat4ls_demo/webapp/src/main/java/uk/co/flax/biosolr/ontology/search/elasticsearch/ElasticDocumentSearch.java
@@ -17,12 +17,21 @@ package uk.co.flax.biosolr.ontology.search.elasticsearch;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.index.query.MultiMatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.script.Script;
+import org.elasticsearch.script.ScriptService;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHitField;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.metrics.cardinality.Cardinality;
+import org.elasticsearch.search.aggregations.metrics.tophits.TopHits;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.co.flax.biosolr.ontology.api.Document;
@@ -46,6 +55,11 @@ public class ElasticDocumentSearch extends ElasticSearchEngine implements Docume
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ElasticDocumentSearch.class);
 
+	private static final String GROUP_FIELD = "study_id";
+	private static final String COUNT_AGGREGATION = "numFound";
+	private static final String HITS_AGGREGATION = "study";
+	private static final String SCORE_AGGREGATION = "topScore";
+
 	private static final String[] DEFAULT_FIELDS = new String[]{
 		"title", "first_author", "publication", "efo_uri.label"
 	};
@@ -58,27 +72,60 @@ public class ElasticDocumentSearch extends ElasticSearchEngine implements Docume
 	public ResultsList<Document> searchDocuments(String term, int start, int rows,
 			List<String> additionalFields, List<String> filters) throws SearchEngineException {
 		// Build the query
-		MultiMatchQueryBuilder qb = QueryBuilders.multiMatchQuery(term, DEFAULT_FIELDS);
+		MultiMatchQueryBuilder qb = QueryBuilders.multiMatchQuery(term, DEFAULT_FIELDS)
+				.minimumShouldMatch("2<25%");
 		if (additionalFields != null && additionalFields.size() > 0) {
 			List<String> parsedAdditional = parseAdditionalFields(additionalFields);
 			parsedAdditional.forEach(qb::field);
 		}
 
-		SearchResponse response = getClient().prepareSearch(getIndexName())
+		/* Build the terms aggregation, since we need a result set grouped by study ID.
+		 * The "top_score" sub-agg allows us to sort by the top score of the results;
+		 * the "study" sub-agg actually pulls back the record data.
+		 * Note that we have to get _all_ rows up to and including the last required, annoyingly. */
+		AggregationBuilder termsAgg = AggregationBuilders.terms(HITS_AGGREGATION)
+				.field(GROUP_FIELD)
+				.order(Terms.Order.aggregation(SCORE_AGGREGATION, false))
+				.size(start + rows)
+				.subAggregation(
+						AggregationBuilders.max(SCORE_AGGREGATION)
+								.script(new Script("_score", ScriptService.ScriptType.INLINE, "expression", null)))
+				.subAggregation(
+						AggregationBuilders.topHits(HITS_AGGREGATION)
+								.setFetchSource("*", "")
+								.addFieldDataField("*")
+								.setFrom(0)
+								.setSize(start + rows));
+
+		// Build the actual search request, including another aggregation to get
+		// the number of unique study IDs returned.
+		SearchRequestBuilder srb = getClient().prepareSearch(getIndexName())
 				.setTypes(getDocumentType())
 				.setQuery(qb)
-				.addFields("*", "_source")
-				.setFrom(start)
-				.setSize(rows)
-				.execute().actionGet();
+				.setSize(0)
+				.addAggregation(termsAgg)
+				.addAggregation(AggregationBuilders.cardinality(COUNT_AGGREGATION).field(GROUP_FIELD));
+		LOGGER.debug("ES Query: {}", srb.toString());
+
+		SearchResponse response = srb.execute().actionGet();
 
 		// Handle the response
-		long total = response.getHits().getTotalHits();
+		long total = ((Cardinality)(response.getAggregations().get(COUNT_AGGREGATION))).getValue();
 		List<Document> docs;
 		if (total == 0) {
 			docs = new ArrayList<>();
 		} else {
-			docs = extractDocuments(response.getHits().getHits());
+			docs = new ArrayList<>(rows);
+			ObjectMapper mapper = buildObjectMapper();
+
+			int lastIdx = (int)(start + rows <= total ? start + rows : total);
+			StringTerms terms = response.getAggregations().get(HITS_AGGREGATION);
+			List<Terms.Bucket> termBuckets = terms.getBuckets().subList(start, lastIdx);
+			for (Terms.Bucket bucket : termBuckets) {
+				TopHits hits = bucket.getAggregations().get(HITS_AGGREGATION);
+				Document doc = extractDocument(mapper, hits.getHits().getAt(0));
+				docs.add(doc);
+			}
 		}
 
 		return new ResultsList<>(docs, start, (start / rows), total);
@@ -88,7 +135,6 @@ public class ElasticDocumentSearch extends ElasticSearchEngine implements Docume
 	public ResultsList<Document> searchByEfoUri(int start, int rows, String term, String... uris) throws SearchEngineException {
 		return null;
 	}
-
 	private List<String> parseAdditionalFields(List<String> additional) {
 		List<String> parsed;
 
@@ -104,47 +150,49 @@ public class ElasticDocumentSearch extends ElasticSearchEngine implements Docume
 		return parsed;
 	}
 
-	private List<Document> extractDocuments(SearchHit[] hits) throws SearchEngineException {
+	private ObjectMapper buildObjectMapper() {
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		List<Document> docs = new ArrayList<>(hits.length);
+		return mapper;
+	}
+
+	private Document extractDocument(ObjectMapper mapper, SearchHit hit) throws SearchEngineException {
+		Document doc;
 
 		try {
-			for (SearchHit hit : hits) {
-				Document doc = mapper.readValue(hit.getSourceAsString(), Document.class);
+			doc = mapper.readValue(hit.getSourceAsString(), Document.class);
 
-				for (Map.Entry<String, SearchHitField> fieldEntry : hit.fields().entrySet()) {
-					if (fieldEntry.getKey().startsWith(getAnnotationField())) {
-						String fieldName = fieldEntry.getKey();
+			for (Map.Entry<String, SearchHitField> fieldEntry : hit.fields().entrySet()) {
+				if (fieldEntry.getKey().startsWith(getAnnotationField())) {
+					String fieldName = fieldEntry.getKey();
 
-						switch (fieldName) {
-							case "efo_uri.label":
-								doc.setEfoLabels(getStringValues(fieldEntry.getValue().getValues()));
-								break;
-							case "efo_uri.child_labels":
-								doc.setChildLabels(getStringValues(fieldEntry.getValue().getValues()));
-								break;
-							case "efo_uri.parent_labels":
-								doc.setParentLabels(getStringValues(fieldEntry.getValue().getValues()));
-								break;
-							default:
-								String shortName = fieldName.substring("efo_uri.".length());
-								if (fieldName.endsWith("_rel_uris")) {
-									doc.getRelatedIris().put(shortName, getStringValues(fieldEntry.getValue().getValues()));
-								} else if (fieldName.endsWith("_rel_labels")) {
-									doc.getRelatedLabels().put(shortName, getStringValues(fieldEntry.getValue().getValues()));
-								}
-						}
+					switch (fieldName) {
+						case "efo_uri.label":
+							doc.setEfoLabels(getStringValues(fieldEntry.getValue().getValues()));
+							break;
+						case "efo_uri.child_labels":
+							doc.setChildLabels(getStringValues(fieldEntry.getValue().getValues()));
+							break;
+						case "efo_uri.parent_labels":
+							doc.setParentLabels(getStringValues(fieldEntry.getValue().getValues()));
+							break;
+						default:
+							String shortName = fieldName.substring("efo_uri.".length());
+							if (fieldName.endsWith("_rel_uris")) {
+								doc.getRelatedIris().put(shortName, getStringValues(fieldEntry.getValue().getValues()));
+							} else if (fieldName.endsWith("_rel_labels")) {
+								doc.getRelatedLabels().put(shortName,
+										getStringValues(fieldEntry.getValue().getValues()));
+							}
 					}
 				}
-
-				docs.add(doc);
 			}
 		} catch (IOException e) {
 			LOGGER.error("Error reading document from source: {}", e.getMessage());
+			throw new SearchEngineException(e);
 		}
 
-		return docs;
+		return doc;
 	}
 
 	private List<String> getStringValues(List<Object> fieldValues) {
