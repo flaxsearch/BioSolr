e0b638a3c0607009b47b86baa424b2cfcac8f84d
Tom Winch <tomw@flax.co.uk>
2016-02-26 16:56:47 +0000

    Include new classes

--- sequence/build.sh
+++ sequence/build.sh
@@ -1,6 +1,6 @@
 cd bin
-jar cvfe fasta.jar uk.co.flax.biosolr.pdbe.fasta.Main uk/co/flax/biosolr/pdbe/fasta org/apache/solr/search/xjoin/SimpleXJoinResultsFactory*.class
+jar cvfe fasta.jar uk.co.flax.biosolr.pdbe.fasta.Main uk/co/flax/biosolr/pdbe/fasta
 jar cvfe phmmer.jar uk.co.flax.biosolr.pdbe.phmmer.Main uk/co/flax/biosolr/pdbe/phmmer
-jar cvf xjoin.jar org/apache/solr/search/xjoin/FieldAppender.class org/apache/solr/search/xjoin/NameConverter.class org/apache/solr/search/xjoin/Combinations*.class org/apache/solr/search/xjoin/JoinSpec*.class org/apache/solr/search/xjoin/XJoin*.class 
+jar cvf xjoin.jar org/apache/solr/search/xjoin/FieldAppender.class org/apache/solr/search/xjoin/NameConverter.class org/apache/solr/search/xjoin/Combinations*.class org/apache/solr/search/xjoin/JoinSpec*.class org/apache/solr/search/xjoin/XJoin*.class org/apache/solr/search/xjoin/simple/Connection.class org/apache/solr/search/xjoin/simple/JsonDocumentFactory.class org/apache/solr/search/xjoin/simple/XmlDocumentFactory.class org/apache/solr/search/xjoin/simple/PathDocument.class org/apache/solr/search/xjoin/simple/SimpleXJoinResultsFactory.class 
 cd ..
 mv bin/*.jar .
